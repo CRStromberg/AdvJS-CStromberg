538b85c6a4fa54969e87adf7c1b0db730f354098
CRStromberg <CRStromberg86@gmail.com>
2020-02-13 16:15:53 -0700

    .

---
+++ Kattis Problems/Akcija/akcija.js
@@ -0,0 +1,53 @@
+//Kattis Akcija program
+/*
+Algo
+    -  Read data
+        *Store first number for N
+        *Store the rest in array
+    -  Write testing function
+    -  Compute Data
+        *Sort array (largest to smallest)
+        *Add two, thow out the third
+            *repeat until end of array
+    -  Output answer
+*/
+"use strict";
+const asrt = require('assert').strict;
+const readline = require('readline');
+
+
+var userinput = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+function test()
+{
+    var test_case_one = [5, 9, 2, 5, 2, 11]; //11 9 5 2 2 = $24
+    var test_case_two = [3, 12, 5, 7];  //12 7 5 = $19
+    var test_case_three = [11, 2, 8, 4, 12, 7, 9, 15, 22, 2, 3, 19]; //22 19 15 12 9 8 7 4 3 2 2 = $77
+    var ans_one = 24;
+    var ans_two = 19;
+    var ans_three = 77;    
+        
+    asrt.strictEqual(ans(test_case_one), ans_one);
+    asrt.strictEqual(ans(test_case_two), ans_two);
+    asrt.strictEqual(ans(test_case_three), ans_three);
+    console.log('all test cases passed.');
+}
+
+function solve()
+{
+
+}
+
+function ans(books)
+{
+
+}
+
+if (require.main == module)
+{
+    if (process.argv.length > 2 && process.argv[2] === 'test') test();
+    else solve();    
+}
