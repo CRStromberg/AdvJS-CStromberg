c0fc7349a4436f76cae2dd56a2a8f90f82f9d438
CRStromberg <CRStromberg86@gmail.com>
2020-02-13 01:09:07 -0700

    Alltest cases passed

---
+++ Kattis Problems/Some Sum/someSum.js
@@ -0,0 +1,64 @@
+//Kattis Some Sum
+/*
+Algo
+    -  Read data
+    -  Write testing function
+    -  Compute Data
+        *1,3,5,7,9 can be either
+        *2,6,10 are odd
+        *4,8 are even
+    -  Output answer
+*/
+
+"use strict";
+const asrt = require('assert').strict;
+const readline = require('readline');
+
+var userinput = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+function test()
+{
+    var inr1 = [4, 10, 7];
+    var out1 = ['even', 'odd', 'either'];
+
+    for (var i = 0; i<3;i++)
+    {
+        var ans = solve(inr1[i]);
+        asrt.strictEqual(out1[i], ans);
+    }
+    console.log('all test cases passed.');
+}
+function solve(user_num)
+{
+    switch(user_num) {
+        case 4:
+        case 8:
+          return 'even';
+          break;
+        case 2:
+        case 6:
+        case 10:
+          return 'odd';
+          break;
+        default:
+          return 'either';
+      } 
+}
+
+if (require.main == module)
+{
+    var answ;
+    if (process.argv.length > 2 && process.argv[2] === 'test') test();
+    else
+    {
+        //Get Numbers from user
+        userinput.question('',function (numin) 
+        {
+            colsole.log(solve(numin));
+            userinput.close();     
+        });
+    }
+}
