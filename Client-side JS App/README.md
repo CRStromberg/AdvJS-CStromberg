be7a83799d6c7f2f376f149c8696e1343a8f48f9
CRStromberg <cstromberg86@gmail.com>
2020-03-31 14:59:57 -0600

    Not finished due to being sick

---
+++ Client-side JS App/README.md
@@ -0,0 +1,7 @@
+# Advanced Programing: JavaScript Client-Side JS App
+
+##Just run the index.html file in your browser to run the program.
+##It is a Tic Tac Toe Game
+- I was sick last week and am struggling with the click events.  I'm sorry it's not fully working.
+- I've tried using a seperate js file and within the html document.
+- I will continue to work on it even though it's late now.
--- Client-side JS App/tictactoe.js
+++ Client-side JS App/tictactoe.js
@@ -1,7 +1,48 @@
-function place_x() {
-    document.getElementById("x").innerHTML = "X";
-  }
+var turn = 0;
+var count = 0;
 
-  function place_o() {
-    document.getElementById("o").innerHTML = "O";
-  }
+function place_x() 
+{
+    document.getElementById("one").innerHTML = "X";
+    document.getElementById("two").innerHTML = "X";
+    document.getElementById("three").innerHTML = "X";
+    document.getElementById("four").innerHTML = "X";
+    document.getElementById("five").innerHTML = "X";
+    document.getElementById("six").innerHTML = "X";
+    document.getElementById("seven").innerHTML = "X";
+    document.getElementById("eight").innerHTML = "X";
+    document.getElementById("nine").innerHTML = "X";
+}
+
+function place_o() 
+{
+    document.getElementById("one").innerHTML = "O";
+    document.getElementById("two").innerHTML = "O";
+    document.getElementById("three").innerHTML = "O";
+    document.getElementById("four").innerHTML = "O";
+    document.getElementById("five").innerHTML = "O";
+    document.getElementById("six").innerHTML = "O";
+    document.getElementById("seven").innerHTML = "O";
+    document.getElementById("eight").innerHTML = "O";
+    document.getElementById("nine").innerHTML = "O";
+}
+
+function main()
+{
+    while(count < 9)
+    {
+        if(turn == 0)
+        {
+            place_x();
+            turn++;
+        }
+        else
+        {
+            place_o;
+            turn--;
+        }
+        count++;
+    }
+
+
+}
