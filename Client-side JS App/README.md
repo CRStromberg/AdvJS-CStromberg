2062d90ea75a52819f12e6a749f5f9328175f885
CRStromberg <cstromberg86@gmail.com>
2020-03-31 16:44:09 -0600

    Final Push of updated program

--- Client-side JS App/README.md
+++ Client-side JS App/README.md
@@ -5,6 +5,6 @@
 - I was sick last week and am struggling with the click events.  I'm sorry it's not fully working.
 - I've tried using a seperate js file and within the html document.
 - I will continue to work on it even though it's late now.
-
-
-- I did finish a basic version, though it's about 30 minutes late.
+- (3:30pm)I did finish a basic version, though it's about 30 minutes late.
+- (4:45)Last update The finished program is in the updated folder, Again I'm sorry it's late
+- I have not commited anything to the main folder past the due date/time except the readme.md
--- Client-side JS App/updatedVersion/script.js
+++ Client-side JS App/updatedVersion/script.js
@@ -1,4 +1,5 @@
 var turn = 0;
+var count = 0;
 
 function place_x(clicked_id) 
 {
@@ -12,19 +13,80 @@ function place_o(clicked_id)
 
 function main(clicked_id)
 {
-    if(turn == 0)
-    {
-        place_x(clicked_id);
-        turn++;
-    }
-    else
-    {
-        place_o(clicked_id);
-        turn--;    
-    }
+    check_content(clicked_id);
 }
 
 function restart()
 {
     location.reload();
+}
+
+function check_content(clicked_id)
+{
+    if(document.getElementById(clicked_id).innerHTML === "")
+    {
+        if(turn == 0)
+        {
+            place_x(clicked_id);
+            turn++;
+        }
+        else
+        {
+            place_o(clicked_id);
+            turn--;    
+        }
+        count++;
+        if(count > 3)
+        {
+            check_winner(count);
+        }
+    }
+}
+
+function check_winner(count)
+{
+    var winner = "";
+    //1,2,3  4,5,6  7,8,9      1,4,7    2,5,8   3,6,9       1,5,9   3,5,7
+    if(document.getElementById("1").innerHTML === document.getElementById("2").innerHTML && document.getElementById("2").innerHTML === document.getElementById("3").innerHTML)
+    {
+        winner = document.getElementById("1").innerHTML;
+        
+    }
+    else if(document.getElementById("4").innerHTML === document.getElementById("5").innerHTML && document.getElementById("5").innerHTML === document.getElementById("6").innerHTML)
+    {
+        winner = document.getElementById("4").innerHTML;
+    }
+    else if(document.getElementById("7").innerHTML === document.getElementById("8").innerHTML && document.getElementById("8").innerHTML === document.getElementById("9").innerHTML)
+    {
+        winner = document.getElementById("7").innerHTML;
+    }
+    else if(document.getElementById("1").innerHTML === document.getElementById("4").innerHTML && document.getElementById("4").innerHTML === document.getElementById("7").innerHTML)
+    {
+        winner = document.getElementById("1").innerHTML;
+    }
+    else if(document.getElementById("2").innerHTML === document.getElementById("5").innerHTML && document.getElementById("5").innerHTML === document.getElementById("8").innerHTML)
+    {
+        winner = document.getElementById("2").innerHTML;
+    }
+    else if(document.getElementById("3").innerHTML === document.getElementById("6").innerHTML && document.getElementById("6").innerHTML === document.getElementById("9").innerHTML)
+    {
+        winner = document.getElementById("3").innerHTML;
+    }
+    else if(document.getElementById("1").innerHTML === document.getElementById("5").innerHTML && document.getElementById("5").innerHTML === document.getElementById("9").innerHTML)
+    {
+        winner = document.getElementById("1").innerHTML;
+    }
+    else if(document.getElementById("3").innerHTML === document.getElementById("5").innerHTML && document.getElementById("5").innerHTML === document.getElementById("7").innerHTML)
+    {
+        winner = document.getElementById("3").innerHTML;
+    }
+
+    if(winner != "")
+    {
+        document.getElementById("win").innerHTML = "The winner is " + winner;
+    }
+    else if(count == 9)
+    {
+        document.getElementById("win").innerHTML = "Draw";
+    }
 }
